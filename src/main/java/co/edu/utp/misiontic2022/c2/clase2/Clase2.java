bc058e5d8da7027314e2f3a556a790ddae308317
svegam23 <svegam23@gmail.com>
2022-07-25 20:05:10 -0500

    1er repositorio

---
+++ src/main/java/co/edu/utp/misiontic2022/c2/clase2/Clase2.java
@@ -0,0 +1,15 @@
+
+
+package co.edu.utp.misiontic2022.c2.clase2;
+
+/**
+ * CLase 22 de Julio de 2022 Grupo 49
+ * 1er Proyecto Maven 
+ * @author vega
+ */
+public class Clase2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
